e5c6f33972e2d0360c3c659cba342a523d0b6e1b
HanBo <hanbo9501@gmail.com>
2024-10-12 16:00:48 +0800

    init(construct): initialize voting code construct

---
+++ src/main/java/cloud/popples/voting/VotingWebApplication.java
@@ -0,0 +1,12 @@
+package cloud.popples.voting;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VotingWebApplication.class, args);
+    }
+}
---
+++ src/test/java/cloud/popples/voting/VotingWebApplicationTest.java
@@ -0,0 +1,13 @@
+package cloud.popples.voting;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class VotingWebApplicationTest {
+
+    @Test
+    public void contextLoads() {
+
+    }
+}
