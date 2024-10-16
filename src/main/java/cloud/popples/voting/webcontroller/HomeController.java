47d6cfd31959a8d0e7559b3f372a6548e67400f1
HanBo <hanbo9501@gmail.com>
2024-10-16 15:02:30 +0800

    feat(homepage): start a homepage in webmvc

---
+++ src/main/java/cloud/popples/voting/webcontroller/HomeController.java
@@ -0,0 +1,19 @@
+package cloud.popples.voting.webcontroller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author 24957
+ */
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping(value = {"/", "/index", "home"})
+    public String index() {
+        return "index";
+    }
+
+}
---
+++ src/test/java/cloud/popples/voting/HomeControllerTest.java
@@ -0,0 +1,27 @@
+package cloud.popples.voting;
+
+import cloud.popples.voting.webcontroller.HomeController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(controllers = {HomeController.class})
+class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testHomePage() throws Exception {
+        mockMvc.perform(get("/home"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"))
+                .andExpect(content().string(containsString("Welcome to")));
+    }
+
+}
